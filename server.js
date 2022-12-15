107cd15e8383c40e11cd5d763b5fbd9031a1c7fb
Nicolas <nicbuirod@hotmail.com>
2022-12-14 20:06:21 -0500

    Ejercicio express

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+let students = {
+  top: {
+    version_top: 26,
+    students: 10,
+    names: [
+      "Felipe",
+      "Laura",
+      "Jailer",
+      "Jaime",
+      "Juan",
+      "Andrés",
+      "Nicolas",
+      "Carlos",
+      "Ivan",
+      "Diana",
+    ],
+  },
+};
+app.get("/My-first-endpoint", (req, res) => {
+  res.status(200).json({
+    message: "Curso encontrado satisfactoriamente",
+    students: students,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
